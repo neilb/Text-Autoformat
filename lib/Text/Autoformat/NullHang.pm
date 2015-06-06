af7f82054ddeda3913cea9613b23d48d4fdecc7e
Neil Bowers <neil@bowers.com>
2015-06-06 21:59:55 +0100

    Split cuckoo packages into separate modules

---
+++ lib/Text/Autoformat/NullHang.pm
@@ -0,0 +1,17 @@
+package Text::Autoformat::NullHang;
+
+use 5.006;
+use strict;
+use warnings;
+
+sub new       { bless {}, $_[0] }
+sub stringify { "" }
+sub length    { 0 }
+sub incr      {}
+sub empty     { 1 }
+sub signature     { "" }
+sub fields { return 0 }
+sub field { return "" }
+sub val { return "" }
+
+1;
